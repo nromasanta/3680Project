e34e210bac24ca42fc8c29fd15c0be83f54fe340
Nicklas Chiang <nicklasmc1@gmail.com>
2024-05-01 19:40:52 -0700

    Create A Quiz Page V1.0

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuth } from "../auth/authProvider";
+import '../styles/Create.css'
 
 const Create = () => {
   const { token, userId } = useAuth();
-  const [question, setQuestion] = useState();
+  const [question, setQuestion, title, setTitle] = useState();
   const [answer, setAnswer] = useState();
   const [questionArray, setQuestionArray] = useState([{question: "", answer: ""}]);
   const [nQuestion, setNQuestion] = useState(0);
@@ -52,26 +53,78 @@ const Create = () => {
   };
 
   return (
-    <div className="temp-page set-container">
-      <form onSubmit={handleSubmit}>
-        {questionArray.map((item, index) => (
-          <div key = {index}>
-            <label>
-              Enter Question {index + 1}:
-              <input
-                type="text"
-                onChange={(e) => setQuestion(e.target.value)}
-              />
-            </label>
-            <label>
-              Enter Answer:
-              <input type="text" onChange={(e) => setAnswer(e.target.value)} />
-            </label>
+    <div className='create-page content set-container'>
+      <div className='create-a-quiz'>
+      <p className='create-page-header'>Create A Quiz</p>
+        <form onSubmit={handleSubmit} className='create-form'>
+          <div className='create-header'>
+            <p className='create-title-label'>Step 1: Title</p>
+            <div className='create-title'>
+                <label className='create-label'>
+                  <input
+                    type='text'
+                    id='text'
+                    className='create-inputs'
+                    placeholder='Quiz Title'
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                  />
+                </label>
+            </div>
           </div>
-        ))}
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={renderNewDiv}>add</button>
+
+          <hr/>
+
+          <div className='create-content'>
+            <p className='create-title-label'>Step 2: Q&A</p>
+            <div className='create-question-container'>
+              {questionArray.map((item, index) => (
+                <div key = {index} className='create-question'>
+                  <label className='create-label'>
+                    <input
+                      type='text'
+                      className='create-inputs'
+                      placeholder='Enter Question: '
+                      onChange={(e) => setQuestion(e.target.value)}
+                      required
+                    />
+                  </label>
+                  <label className='create-label'>
+                    <input 
+                    type='text' 
+                    className='create-inputs' 
+                    placeholder='Enter Answer: '
+                    onChange={(e) => setAnswer(e.target.value)} 
+                    required
+                    />
+                  </label>
+                </div>
+              ))}
+            </div>
+            <button onClick={renderNewDiv}>
+              <p className='login-btn create-add-button'>
+                Add Another Question
+              </p>
+            </button>
+          </div>
+          
+          <hr/>
+
+          <div className='create-tags'>
+            <p className='create-title-label'>Step 3: Tags</p>
+          </div>
+
+          <hr/>
+
+          <div className='create-button-container'>
+            <button type='submit'>
+              <p className='login-btn create-button'>
+                Create Quiz
+              </p>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );};
 
